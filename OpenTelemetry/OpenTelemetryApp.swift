8732baa0982ee7c355fe9fce57f7ed444b450c67
Luis Pineda <luis.pinedahenriquez@externos-cl.cencosud.com>
2024-01-19 09:13:22 -0500

    Initial Commit

---
+++ OpenTelemetry/OpenTelemetryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTelemetryApp.swift
+//  OpenTelemetry
+//
+//  Created by Luis Pineda on 19/01/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenTelemetryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
