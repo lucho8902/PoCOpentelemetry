2891a2bc48b78d8e920e64f9f95b90839c9c8403
Luis Pineda <luis.pinedahenriquez@externos-cl.cencosud.com>
2024-02-02 09:55:18 -0500

    lo que necesto

---
+++ OpenTelemetry/File.swift
@@ -0,0 +1,276 @@
+//
+//  File.swift
+//  OpenTelemetry
+//
+//  Created by Luis Pineda on 2/02/24.
+//
+
+import Foundation
+/**
+
+/* Begin XCSwiftPackageProductDependency section */
+        20E4C5082B6C2F3F001A4DF2 /* DatadogExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = DatadogExporter;
+        };
+        20E4C50A2B6C2F3F001A4DF2 /* InMemoryExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = InMemoryExporter;
+        };
+        20E4C50C2B6C2F3F001A4DF2 /* JaegerExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = JaegerExporter;
+        };
+        20E4C50E2B6C2F3F001A4DF2 /* NetworkStatus */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = NetworkStatus;
+        };
+        20E4C5102B6C2F3F001A4DF2 /* OpenTelemetryApi */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = OpenTelemetryApi;
+        };
+        20E4C5122B6C2F3F001A4DF2 /* OpenTelemetryProtocolExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = OpenTelemetryProtocolExporter;
+        };
+        20E4C5142B6C2F3F001A4DF2 /* OpenTelemetryProtocolExporterHTTP */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = OpenTelemetryProtocolExporterHTTP;
+        };
+        20E4C5162B6C2F3F001A4DF2 /* OpenTelemetrySdk */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = OpenTelemetrySdk;
+        };
+        20E4C5182B6C2F3F001A4DF2 /* OpenTracingShim-experimental */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = "OpenTracingShim-experimental";
+        };
+        20E4C51A2B6C2F3F001A4DF2 /* PersistenceExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = PersistenceExporter;
+        };
+        20E4C51C2B6C2F3F001A4DF2 /* PrometheusExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = PrometheusExporter;
+        };
+        20E4C51E2B6C2F3F001A4DF2 /* ResourceExtension */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = ResourceExtension;
+        };
+        20E4C5202B6C2F3F001A4DF2 /* SignPostIntegration */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = SignPostIntegration;
+        };
+        20E4C5222B6C2F3F001A4DF2 /* StdoutExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = StdoutExporter;
+        };
+        20E4C5242B6C2F3F001A4DF2 /* SwiftMetricsShim */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = SwiftMetricsShim;
+        };
+        20E4C5262B6C2F3F001A4DF2 /* URLSessionInstrumentation */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = URLSessionInstrumentation;
+        };
+        20E4C5282B6C2F3F001A4DF2 /* ZipkinExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = ZipkinExporter;
+        };
+        20E4C52A2B6C2F3F001A4DF2 /* OTLPExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = OTLPExporter;
+        };
+        20E4C52C2B6C2F3F001A4DF2 /* OTLPHTTPExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = OTLPHTTPExporter;
+        };
+        20E4C52E2B6C2F3F001A4DF2 /* loggingTracer */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = loggingTracer;
+        };
+        20E4C5302B6C2F3F001A4DF2 /* simpleExporter */ = {
+            isa = XCSwiftPackageProductDependency;
+            package = 20E4C5072B6C2F3F001A4DF2 /* XCRemoteSwiftPackageReference "opentelemetry-swift" */;
+            productName = simpleExporter;
+        };
+/* End XCSwiftPackageProductDependency section */
+
+ 
+ 
+ 
+ ----------------------------------------------------------
+
+ {
+   "pins" : [
+     {
+       "identity" : "grpc-swift",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/grpc/grpc-swift.git",
+       "state" : {
+         "revision" : "6ade19f0b57f5fc436dfecfced83f3c84d1095b9",
+         "version" : "1.21.0"
+       }
+     },
+     {
+       "identity" : "opentelemetry-swift",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/open-telemetry/opentelemetry-swift",
+       "state" : {
+         "branch" : "main",
+         "revision" : "0033445b1de9516c16b75d678b0003a8b5ec1c8b"
+       }
+     },
+     {
+       "identity" : "opentracing-objc",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/undefinedlabs/opentracing-objc",
+       "state" : {
+         "revision" : "18c1a35ca966236cee0c5a714a51a73ff33384c1",
+         "version" : "0.5.2"
+       }
+     },
+     {
+       "identity" : "reachability.swift",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/ashleymills/Reachability.swift",
+       "state" : {
+         "revision" : "c01127cb51f591045696128effe43c16840d08bf",
+         "version" : "5.2.0"
+       }
+     },
+     {
+       "identity" : "swift-atomics",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-atomics.git",
+       "state" : {
+         "revision" : "cd142fd2f64be2100422d658e7411e39489da985",
+         "version" : "1.2.0"
+       }
+     },
+     {
+       "identity" : "swift-collections",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-collections.git",
+       "state" : {
+         "revision" : "d029d9d39c87bed85b1c50adee7c41795261a192",
+         "version" : "1.0.6"
+       }
+     },
+     {
+       "identity" : "swift-log",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-log.git",
+       "state" : {
+         "revision" : "e97a6fcb1ab07462881ac165fdbb37f067e205d5",
+         "version" : "1.5.4"
+       }
+     },
+     {
+       "identity" : "swift-metrics",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-metrics.git",
+       "state" : {
+         "revision" : "971ba26378ab69c43737ee7ba967a896cb74c0d1",
+         "version" : "2.4.1"
+       }
+     },
+     {
+       "identity" : "swift-nio",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-nio.git",
+       "state" : {
+         "revision" : "635b2589494c97e48c62514bc8b37ced762e0a62",
+         "version" : "2.63.0"
+       }
+     },
+     {
+       "identity" : "swift-nio-extras",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-nio-extras.git",
+       "state" : {
+         "revision" : "fb70a0f5e984f23be48b11b4f1909f3bee016178",
+         "version" : "1.19.1"
+       }
+     },
+     {
+       "identity" : "swift-nio-http2",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-nio-http2.git",
+       "state" : {
+         "revision" : "0904bf0feb5122b7e5c3f15db7df0eabe623dd87",
+         "version" : "1.30.0"
+       }
+     },
+     {
+       "identity" : "swift-nio-ssl",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-nio-ssl.git",
+       "state" : {
+         "revision" : "7c381eb6083542b124a6c18fae742f55001dc2b5",
+         "version" : "2.26.0"
+       }
+     },
+     {
+       "identity" : "swift-nio-transport-services",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-nio-transport-services.git",
+       "state" : {
+         "revision" : "6cbe0ed2b394f21ab0d46b9f0c50c6be964968ce",
+         "version" : "1.20.1"
+       }
+     },
+     {
+       "identity" : "swift-protobuf",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-protobuf.git",
+       "state" : {
+         "revision" : "65e8f29b2d63c4e38e736b25c27b83e012159be8",
+         "version" : "1.25.2"
+       }
+     },
+     {
+       "identity" : "swift-system",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/apple/swift-system.git",
+       "state" : {
+         "revision" : "025bcb1165deab2e20d4eaba79967ce73013f496",
+         "version" : "1.2.1"
+       }
+     },
+     {
+       "identity" : "thrift-swift",
+       "kind" : "remoteSourceControl",
+       "location" : "https://github.com/undefinedlabs/Thrift-Swift",
+       "state" : {
+         "revision" : "18ff09e6b30e589ed38f90a1af23e193b8ecef8e",
+         "version" : "1.1.2"
+       }
+     }
+   ],
+   "version" : 2
+ }
+
+ 
+
+ **/
